e24563d49a4a0191318b466d04a8168e66e6ea8d
The Arts Network <artfrontier>
2018-08-15 15:10:05 +0530

    Independence day releasev01

---
+++ assets/js/js-course.js
@@ -0,0 +1,26 @@
+function myFuction(x, y) {
+    
+    var z = x + y;
+    
+    console.log(z);
+    
+};
+
+myFuction(100, 200);
+
+var myName = "Aks";
+var mySurname = "Jirage";
+
+console.assert()
+
+(function newFunction(){
+    
+})();
+
+if (myName == "Aks") {
+    
+    document.write("Pls tell surname now. ")
+    
+    } else {
+        document.write("Pls tell surname now. ")
+    };
